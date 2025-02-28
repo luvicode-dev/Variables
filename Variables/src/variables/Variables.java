601e0c1a9099f7a274c4c3d30f9fca5a04f7f21f
Luvi  Code <luvicodes@gmail.com>
2025-02-28 11:29:47 -0500

    Add files via upload

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,30 @@
+
+package variables;
+/**
+ *
+ * @author luvi
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        // Variable de Tipo Entero 
+        int telefono = 60347895;
+        // Variable de Tipo Char
+        char letra = 'G';
+        /* Variable de Tipo Float
+        Es necesario realizar un casheo (f)*/
+        float peso = 65.2f;
+        /*Variable de Tipo Doubke
+        Es importante para tener mas precision con 
+        numeros decimales*/
+        double nGrande = 76.901820;
+        
+    // Imprimiendo el contenido de las variables (telefono,letra,peso,Ngrande)
+        System.out.println("El numero de telefono es :" + telefono);
+        System.out.println("La letra es :" + letra);
+        System.out.println("El peso es :" + peso);
+        System.out.println("El numero es grande :" + nGrande);      
+        
+    }
+    
+}
